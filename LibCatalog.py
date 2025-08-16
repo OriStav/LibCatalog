30654ac90e5fbf1a4d50cfe2be68fc761cc14762
OriStav <111699131+OriStav@users.noreply.github.com>
2025-08-16 09:26:54 +0300

    names

--- LibCatalog.py
+++ LibCatalog.py
@@ -7,7 +7,7 @@ TODO:
 import streamlit as st
 import pandas as pd
 from utils import render_books_tab, data_loader
-st.set_page_config(page_title="ספרייה קהילתית", page_icon="📚", layout="wide")
+st.set_page_config(page_title="קטלוג ספרייה קהילתית", page_icon="📚", layout="wide")
 with st.spinner("Loading...", show_time=True):
     books_df, loans_df = data_loader()
 
