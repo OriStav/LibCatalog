b97489539b4bcb9f7d79a83639bca0a20fced6c9
OriStav <111699131+OriStav@users.noreply.github.com>
2025-08-16 13:44:36 +0300

    updated template 01

--- LibCatalog.py
+++ LibCatalog.py
@@ -1,14 +1,18 @@
 """
-streamlit run app.py
+streamlit run LibCatalog.py
 
-TODO:
-- backup backslash
 """
 import streamlit as st
 import pandas as pd
-from utils import render_books_tab, data_loader
+from utils import render_books_search_and_table, data_loader, center_header
 st.set_page_config(page_title="קטלוג ספרייה קהילתית", page_icon="📚", layout="wide")
 with st.spinner("Loading...", show_time=True):
     books_df, loans_df = data_loader()
 
-render_books_tab(books_df, loans_df)
+center_header(level=1, text="ספריית שדי חמד 🌳")
+
+render_books_search_and_table(books_df, loans_df)
+st.markdown(
+    "<div style='text-align: center;'><a href='mailto:hemmed.library@gmail.com'>צור קשר</a> 📥</div>",
+    unsafe_allow_html=True
+)
--- utils.py
+++ utils.py
@@ -13,6 +13,10 @@ def data_loader():
     books_df["available"] = ~books_df['id'].isin(loaned_book_ids)
     books_df['available'] = np.where(books_df['available'], '✅ זמין', '❌ לא זמין')
     books_df = books_df.query('active == True').drop(columns=['active'])
+    books_df['name'] = books_df['name'].str.strip()
+    books_df['author'] = books_df['author'].str.strip()
+
+    books_df['combination'] = books_df['name'] + " - " + books_df['author']
     return  books_df, loans_df
 
 def calculate_metrics(books_df, loans_df):
@@ -32,8 +36,7 @@ def calculate_metrics(books_df, loans_df):
     borrowed_books = len(active_loans['book_id'].unique())
     late_books = len(late_loans['book_id'].unique())
     available_books = total_books - borrowed_books
-    
-    return {
+    metrics ={
         'active_loaners': active_loaners_count,
         'late_loaners': late_loaners_count,
         'active_loans': len(active_loans),
@@ -43,27 +46,12 @@ def calculate_metrics(books_df, loans_df):
         'late_books': late_books,
         'available_books': available_books
     }
-
-def render_books_tab(books_df, loans_df):
-    """Render the books tab content"""
-    metrics = calculate_metrics(books_df, loans_df)
-    center_header(level=1, text="🌳 קטלוג ספרים 📚")
-    center_header(level=3, text="ספריית שדי חמד")
-    render_books_search_and_table(books_df, loans_df)
     with st.container(horizontal=True, horizontal_alignment="center"):
         st.metric("📚 סה״כ ספרים", f"{metrics['total_books']:,}")
         st.metric("✅ ספרים זמינים", f"{metrics['available_books']:,}")
         st.metric("📖 ספרים מושאלים", f"{metrics['borrowed_books']:,}")
 
-def render_books_search_and_table(books_df, loans_df):
-    """Render the books search and table section"""
-    # Search and filter
-    books_df = books_df.copy()
-    center_header(level=6, text="✍️ חיפוש ספרים לפי שם או מחבר 🔎")
-    search_term = st.selectbox("חיפוש ספרים לפי שם או מחבר", 
-                                 options=[''] + sorted(books_df['name'].unique().tolist() + books_df['author'].unique().tolist()),
-                                 placeholder='בחר/י ספר או מחבר',label_visibility="collapsed")
-    
+def expanded_status(books_df, loans_df):# deprecated
     # Calculate book status
     active_loans = loans_df[loans_df['return_date'].isna()].copy()
     active_loans.loc[:, 'loan_duration'] = (datetime.today() - pd.to_datetime(active_loans['loan_date'], format='%d/%m/%Y')).dt.days
@@ -78,35 +66,89 @@ def render_books_search_and_table(books_df, loans_df):
 
     # Add status to books_df
     books_df.loc[:, 'status'] = books_df['id'].apply(lambda x: book_status.get(x, '✅ זמין'))
+    return books_df
+
+def table_render(books_df, search_term=None):
     # Filter books based on search and category
     if search_term:
         filtered_books = books_df[
-            books_df['name'].str.contains(search_term, case=False) |
-            books_df['author'].str.contains(search_term, case=False)
+            books_df['combination'] == search_term
+            # books_df['name'].str.contains(search_term, case=False) |
+            # books_df['author'].str.contains(search_term, case=False)
         ]
     else:
         filtered_books = books_df
 
     # Configure columns for books table
     books_columns = {
-        # 'category': st.column_config.TextColumn('🏷️ קטגוריה', width='medium'),
-        'available': st.column_config.TextColumn('📊 זמינות', width='50px'),
-        # 'status': st.column_config.TextColumn('🩺 סטטוס', width='medium'),
+        'available': st.column_config.TextColumn('👀 זמינות', width='40px'),
         'author': st.column_config.TextColumn('✍️ מחבר', width='50px'),
         'name': st.column_config.TextColumn('📖 שם הספר', width="9px"),
+        # 'status': st.column_config.TextColumn('🩺 סטטוס', width='medium'),
         # 'id': st.column_config.NumberColumn('🔢 מזהה', width='small'),
+        # 'category': st.column_config.TextColumn('🏷️ קטגוריה', width='medium'),
         # 'active': st.column_config.CheckboxColumn('🟢 פעיל', width='small'),
     }
-    df_show = filtered_books[filtered_books.columns[::-1]].drop(columns=["id"])
+    df_show = books_df[books_df.columns[::-1]].drop(columns=["id"])
     st.dataframe(
-        df_show.sort_values(["name","author"]),
+        df_show.sort_values(["author","name"],ascending=True),
         column_config=books_columns,
         column_order=books_columns.keys(),
         hide_index=True,
         use_container_width=True,
     )
+    return filtered_books
 
+def render_books_search_and_table(books_df, loans_df):
+    """Render the books search and table section"""
+    # Search and filter
+    # books_df = books_df.copy()
+
+    with st.form(key="form_search",border=False,enter_to_submit=True,clear_on_submit=True):
+        st.session_state.dynamic_key = f"search_{0}" if "dynamic_key" not in st.session_state else st.session_state.dynamic_key
+        search_options = [''] + sorted(books_df['combination'],reverse=True)
+
+        search_term = st.selectbox("חיפוש ספרים לפי שם או מחבר",
+                                    options=search_options,
+                                    placeholder='בחר/י ספר או מחבר', label_visibility="collapsed",key="search_selectbox")
+        search_row = st.container(horizontal=True, horizontal_alignment="center")
+        
+        status_cont = st.container(horizontal_alignment="center",
+                                height=140, border=False,
+                                vertical_alignment="center",key="dynamic_status")
+        st.divider()
+        center_header(level=5, text="קטלוג הספרים 📚")
+        df_show = table_render(books_df, search_term)
+
+        if not search_term:
+            status_cont.markdown(
+                    """
+                    <div style='display: flex; justify-content: center;'>
+                        <div style='background-color: #fcf7e4; color: #856404; border: 1px solid #ffeeba; border-radius: 8px; padding: 10px 20px; margin: 10px 0; font-size: 1em; text-align: center; max-width: 400px;'>
+                             יש להקליד שם ספר או מחבר ולהקיש 🔎 לחיפוש
+                        </div>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+            )
+
+        if search_row.form_submit_button(":material/search:", type="primary"):
+            # if len(df_show) == 1:
+            with status_cont:
+                center_header(5, f"{df_show['name'].values[0]}")
+                center_header(6, f"{df_show['author'].values[0]}")
+                center_header(7, f"{df_show['available'].values[0]}")
+
+        if search_row.form_submit_button(":material/refresh:", type="primary"):
+            
+            search_term = ""
+        calculate_metrics(books_df, loans_df)
+        
+    # with table_cont:
 
 def center_header(level=1, text="📚 ספרייה קהילתית 🌳"):
     """level >= 7 is not bold"""
-    st.markdown(f"<h{level} style='text-align: center;'>{text}</h{level}>", unsafe_allow_html=True)
+    if level >= 7:
+        st.markdown(f"<div style='text-align: center;'>{text}</div>", unsafe_allow_html=True)
+    else:
+        st.markdown(f"<h{level} style='text-align: center;'>{text}</h{level}>", unsafe_allow_html=True)
